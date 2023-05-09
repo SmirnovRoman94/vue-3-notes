e6493d95f2365edfa48c6987d29a0bb95cd61f9d
Roman Smirnov <smirnow3217831@gmail.com>
2023-05-09 14:01:35 +0300

    vue cli init

---
+++ src/components/MyFooter.vue
@@ -0,0 +1,22 @@
+<template>
+    <footer class="footer">
+      <div class="container">
+          <ul class="navbar-list">
+              <li class="navbar-item" v-for="link in linksItem" :key="link.alias">
+              <router-link class="navbar-link" :to="link.url">{{
+                  link.title
+              }}</router-link>
+              </li>
+          </ul>
+      </div>
+</footer>
+</template>
+
+<script>
+import {links} from '@/_config'
+export default{
+  data: ()=>({
+      linksItem: links
+  }),
+}
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    <p>Home page</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+
+};
+</script>
